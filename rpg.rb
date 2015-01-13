f44bbb9046d487e32f201b299772a683ef29d26f
Alvin Liang <liangalvin@yahoo.com>
2015-01-12 22:13:03 -0500

    unpulled version from myself

--- rpg.rb
+++ rpg.rb
@@ -13,7 +13,7 @@ end
 
 gamer_name = gets.chomp.capitalize!
 
-gamer = User.new(gamer_name) #username set to gamer
+gamer = User.new gamer_name #username set to gamer
 
 class Villian
   attr_accessor :name, :hp, :hit_chance
@@ -29,7 +29,7 @@ cpu = Villian.new() #every bad guy is "beast"
 #villians = ["semi-colon", "Justin Bieber", "Darth Vader", "Bizzaro #{gamer.name}"]
 
 # *ask the fight question
-def fight_question
+def fight_question(gamer, cpu)
   fighting = true
   while fighting #aslong as fightin = true
     puts "Fight the beast or flee?! (Type 'fight' or 'flee')"
@@ -38,7 +38,7 @@ def fight_question
       fight(gamer, cpu)
       fighting = false
     elsif fight_flee == "flee"
-      flee()
+      flee(gamer)
       fighting = false
     else
       puts "Unable to read command"
@@ -48,46 +48,60 @@ end
 
 #when fight is called
 def fight(gamer, cpu)
-keep_fighting = true
+  keep_fighting = true
   while gamer.hp > 0 && cpu.hp > 0
     gamer_chance = rand(0..100)
     cpu_chance = rand(0..100)
-    if gamer.hp < 0
-      if gamer_chance > 81
+    #puts "1. checkpoint"
+    #puts gamer_chance
+    if gamer.hp > 0
+      #puts gamer_chance
+      #puts "2. checkpoint <<<<<<< THE IF STATEMENT IS BROKEN"
+      if gamer_chance < 81
+        puts gamer_chance
         damage = rand(1..6)
-        if damage == 6
-          puts "Fatal blow!"
-          cpu.hp = 0
-          keep_fighting = false
-        else
-          puts "You did #{damage}"
-          cpu.hp -= damage
-          puts "The beast lost #{damage} hp"
-        end
+          if damage == 6
+            puts "Fatal blow!"
+            cpu.hp = 0
+            keep_fighting = false
+          else
+            puts "You did #{damage} damage"
+            cpu.hp -= damage
+            sleep 1
+            puts "The beast lost #{damage} hp"
+          end
+         #end
+      elsif gamer_chance > 80
+          puts "#{gamer.name} with #{gamer.hp} HP, you missed the attack, looks like cake not only adds to the waistline but also to the arms..."
+          #puts gamer_chance
+          sleep 1
       end
-    else
-        puts "You missed the attack, looks like cake not only adds to the waistline but also to the arms..."
+    elsif gamer.hp == 0
+      puts "place holder"
     end
-    if cpu.hp < 0
-      if cpu_chance > 61
+  #end
+    if cpu.hp > 0
+      if cpu_chance < 61
         damage = rand (2..4)
         if damage == 4
-          puts "Critical hit! The beast did #{damage}!"
+          puts "Critical hit! The beast did #{damage} damage!"
           gamer.hp -= damage
         else
-          puts "The foul beast did #{damage}"
+          puts "The foul beast did #{damage} damage"
           gamer.hp -= damage
         end
+      #end
+      else
+        puts "The beast missed their attack! Quickly send this foul beast back to the abyss!"
       end
-    else
-      puts "The beast missed their attack! Quickly send this foul beast back to the abyss!"
     end
   end
 end
 
-def flee
+def flee(gamer)
 
-  puts "You lost control of their bowels and fled"
+  gamer.hp -= 2
+    puts "You lost control of your bowels and fled with #{gamer.hp} HP and a trail of feces to follow"
 
 end
 
@@ -98,11 +112,10 @@ puts "You encounter your first problem of the day,brush your teeth or eat breakf
 first_answer = gets.chomp.downcase
 if first_answer == "first"
   puts "You go to the bathroom BUT SUDDENLY AN ALLIGATOR APPEARS FROM THE TOILET!"
-  fight_question()
-elsif
-  fist_answer == "second"
+  fight_question(gamer, cpu)
+elsif first_answer == "second"
   puts "You go make breakfast BUT SUDDENLY A Racoon HAS YOUR BREAKFAST!"
-  fight_question()
+  fight_question(gamer, cpu)
 end
 
 
@@ -111,10 +124,10 @@ puts "Second problem now arises, what is for lunch? (Type in 'chicken' or 'fish'
 second_answer = gets.chomp.downcase
 if second_answer == "chicken"
   puts "The chicken is still alive and challenges you to a duel!"
-  fight_question()
-elsif seond_answer == "second"
+  fight_question(gamer, cpu)
+elsif seond_answer == "fish"
   puts "The fish throws it's scales at your like a ninja star!"
-  fight_question()
+  fight_question(gamer, cpu)
 end
 
 puts "Seriously #{gamer.name}! You've wasted a good amount of your time on a terminal game about real life..."
